2bd3e88fc876637e3aa93111ccb260deb0e39491
ifadi <fadi@asbih.com>
2023-11-14 11:07:24 +0200

    add fix for ilias 8.6

--- classes/class.ilLUHShibAuthPlugin.php
+++ classes/class.ilLUHShibAuthPlugin.php
@@ -58,7 +58,7 @@ class ilLUHShibAuthPlugin extends ilShibbolethAuthenticationPlugin
      */
     public function beforeCreateUser(ilObjUser $user): ilObjUser
     {
-        $this->getLogger()->debug('Before user creation');
+        $this->logger->debug('Before user creation');
         $user = $this->updateMatriculation($user, true);
         $user = $this->createCustomShibLogin($user);
 
@@ -71,7 +71,7 @@ class ilLUHShibAuthPlugin extends ilShibbolethAuthenticationPlugin
      */
     public function beforeUpdateUser(ilObjUser $user): ilObjUser
     {
-        $this->getLogger()->debug('Before user update');
+        $this->logger->debug('Before user update');
         $user = $this->updateMatriculation($user, false);
 
         return $user;
@@ -94,10 +94,10 @@ class ilLUHShibAuthPlugin extends ilShibbolethAuthenticationPlugin
         // if ext_account is not empty, use it as login
         if (!empty($login)) {
             $user->setLogin($login);
-            $this->getLogger()->debug('ext_accout is found, username is being set now as the LUH-ID:' . $user->getExternalAccount());
+            $this->logger->debug('ext_accout is found, username is being set now as the LUH-ID:' . $user->getExternalAccount());
         }
         else { // else if ext_account is empty do nothing, uses ILIAS default login scheme
-            $this->getLogger()->debug('ext_account is not found, falling back to the ILIAS default login');
+            $this->logger->debug('ext_account is not found, falling back to the ILIAS default login');
         }
 
         return $user;
@@ -112,44 +112,36 @@ class ilLUHShibAuthPlugin extends ilShibbolethAuthenticationPlugin
     {
         $shib_mn_field = $this->settings->get(self::SHIB_MATRICULATION_FIELD, '');
         if (!strlen($shib_mn_field)) {
-            $this->getLogger()->debug('No matriculation number mapping configured');
+            $this->logger->debug('No matriculation number mapping configured');
             return $user;
         }
 
         $shib_mn_update = $this->settings->get(self::SHIB_MATRICULATION_UPDATE, 0 );
         if (!$is_creation_mode && !$shib_mn_update) {
-            $this->getLogger()->debug('No update configured for matriculation in global settings');
+            $this->logger->debug('No update configured for matriculation in global settings');
             return $user;
         }
 
         if (array_key_exists($shib_mn_field, $_SERVER)) {
             $shib_mn_value = trim($_SERVER[$shib_mn_field]);
             if (!strlen($shib_mn_value)) {
-                $this->getLogger()->debug('No matriculation number send by shib server.');
+                $this->logger->debug('No matriculation number send by shib server.');
                 return $user;
             }
 
             $shib_mn_parts = explode(':', $shib_mn_value);
             if($shib_mn_parts === false) {
-                $this->getLogger()->debug('Cannot parse matriculation number: ' . $shib_mn_value);
+                $this->logger->debug('Cannot parse matriculation number: ' . $shib_mn_value);
                 return $user;
             }
 
             $matriculation = end($shib_mn_parts);
-            $this->getLogger()->debug('Update matriculation number: ' . $matriculation);
+            $this->logger->debug('Update matriculation number: ' . $matriculation);
             $user->setMatriculation($matriculation);
         }
         else {
-            $this->getLogger()->debug('No matriculation number found. ');
+            $this->logger->debug('No matriculation number found. ');
         }
         return $user;
     }
-    private function getLogger() {
-        if ($this->getLogger() === null) {
-            global $DIC;
-            $this->logger = $DIC->logger()->auth();
-        }
-        return $this->logger;
-    }
-
 }
